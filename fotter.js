65e3578b6f6fd71ab5669e54902d6568a0df19d9
Nakka Renuka Devi <121855513+nakkarenukadevi@users.noreply.github.com>
2023-03-06 21:11:19 +0530

    Add files via upload

---
+++ fotter.js
@@ -0,0 +1,10 @@
+function Fotter() {
+  return (
+    <>
+      <div className="fotter">
+        <p>copywriteby@renukadevi</p>
+      </div>
+    </>
+  );
+}
+export default Fotter;
---
+++ header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1>SriLakshmi Store</h1>
+      </div>
+    </>
+  );
+}
+export default Header;
